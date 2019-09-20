72abcf0ad021a7814902858f41225788fae8c9a4
Indrajit V <indrajit@weinvest.net>
2019-09-20 23:02:51 +0530

    changes

--- src/pages/RootFolder.jsx
+++ src/pages/RootFolder.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import TreeView from "../components/common/TreeView";
 import FolderActions from "../actions/FolderActions";
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import folder from './folder';
 
-const PortfolioApp = combineReducers({
+const folderStructureApp = combineReducers({
     data : folder
 });
-export default PortfolioApp;
+export default folderStructureApp;
